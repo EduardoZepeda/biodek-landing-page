136dbdeec7b22ede061986c2e586c35fca5cba34
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-03-23 22:51:34 -0600

    Add about component with clipped image

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import LoginForm from './components/LoginForm'
+import stylesAbout from '../styles/About.module.css'
 
 function About() {
     return (
@@ -11,9 +9,16 @@ function About() {
                 <meta name="login" content="About or magical project" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <div>
-
+            <div className={stylesAbout.about}>
+                <div className={stylesAbout.intro}>
+                    <h2>About us</h2>
+                    <p>Welcome to Biodek, a company that specializes in organizing biochemistry conferences for professionals and researchers in the field. We strive to bring together experts from academia, industry, and government to discuss the latest advancements in biochemistry and related fields. Our conferences provide a platform for attendees to share their research, exchange ideas, and build collaborations that lead to groundbreaking discoveries. With a team of experienced organizers and a commitment to excellence, we ensure that our events are informative, engaging, and memorable. Join us at our next conference and be a part of the exciting world of biochemistry.</p>
+                </div>
+                <div className={stylesAbout.pictureContainer}>
+                    <img className={stylesAbout.imgBg} src={'about/main.jpg'} alt="" />
+                </div>
             </div>
+
         </>
     )
 }
