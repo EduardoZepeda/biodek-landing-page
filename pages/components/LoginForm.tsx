d38998ab542ee0c18e4c01f07d5fe092495d28e8
Eduardo Zepeda <eduardozepeda@coffeebytes.dev>
2023-03-24 12:20:34 -0600

    Add real fetch request to login and password rec

--- pages/components/LoginForm.tsx
+++ pages/components/LoginForm.tsx
@@ -18,11 +18,24 @@ const LoginForm = () => {
         if (event.target.name == 'password') { setPassword(event.target.value) }
     }
 
-    const handleSubmit = (event: React.FormEvent): void => {
+    const handleSubmit = async (event: React.FormEvent) => {
+        // easier with react query but let's keep it simple
         event.preventDefault()
         setEmail('')
         setPassword('')
-        setError('An account with that password and email doesn\'t exist')
+        const response = await fetch('/api/login', {
+            method: 'POST',
+            body: JSON.stringify({ email, password }), // data can be `string` or {object}!
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => {
+            if (res.status === 400) {
+                setError('Your credentials were invalid')
+            }
+            return res.json()
+        }).catch(error => setError(error))
+
     }
 
     useEffect(() => {
@@ -34,9 +47,9 @@ const LoginForm = () => {
             <h2>Login</h2>
             <form className={stylesForm.formContainer} onSubmit={handleSubmit} action=''>
                 <label htmlFor="email">Email</label>
-                <input onChange={handleChange} className={stylesForm.formInput} name="email" id="email" type="email" value={email} required />
+                <input onChange={handleChange} className={stylesForm.formInput} name="email" id="email" type="email" value={email} placeholder={'your-email@email.com'} required />
                 <label htmlFor="password">Password</label>
-                <input onChange={handleChange} className={stylesForm.formInput} name="password" id="password" type="password" value={password} required />
+                <input onChange={handleChange} className={stylesForm.formInput} name="password" id="password" type="password" placeholder={'your-password'} value={password} required />
                 <Link href="/password-recover">Forgot your password?</Link>
                 <button type='submit' className={`${stylesButton.btn} ${stylesButton.loginLogout}`}>Login</button>
                 <p className={stylesForm.formError}>{error}</p>
--- pages/components/RecoverPasswordForm.tsx
+++ pages/components/RecoverPasswordForm.tsx
@@ -5,17 +5,32 @@ import stylesButton from '../../styles/Buttons.module.css'
 const RecoverPasswordForm = () => {
     const [email, setEmail] = useState<string>('');
     const [error, setError] = useState<string>('');
+    const [message, setMessage] = useState<string>('');
 
     const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
         // YAGNI, for now this is enough
         setError('')
+        setMessage('')
         setEmail(event.target.value)
     }
 
-    const handleSubmit = (event: React.FormEvent) => {
+    const handleSubmit = async (event: React.FormEvent) => {
+        // easier with react query but let's keep it simple
         event.preventDefault()
         setEmail('')
-        setError('We will send password recovery instructions if that email exists')
+        const response = await fetch('/api/password-recovery', {
+            method: 'POST',
+            body: JSON.stringify({ email }), // data can be `string` or {object}!
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => {
+            // If email is valid this view always returns a 200 status
+            if (res.status === 200) {
+                setMessage('We will send password recovery instructions if that email exists')
+            }
+            return res.json()
+        }).catch(error => setError(error))
     }
 
     useEffect(() => {
@@ -27,9 +42,10 @@ const RecoverPasswordForm = () => {
             <h2>Recover Password</h2>
             <form onSubmit={handleSubmit} className={stylesForm.formContainer} action=''>
                 <label htmlFor="email">Write down the mail you used to register and we'll help you recover your password</label>
-                <input onChange={handleChange} className={stylesForm.formInput} id="email" type="email" minLength={4} required />
+                <input onChange={handleChange} className={stylesForm.formInput} id="email" type="email" minLength={4} value={email} placeholder={'your-email@email.com'} required />
                 <button type='submit' className={`${stylesButton.btn} ${stylesButton.loginLogout}`}>Recover password</button>
-                <p className={stylesForm.formSuccess}>{error}</p>
+                {message !== '' ? <p className={stylesForm.formSuccess}>{message}</p> : null}
+                {error !== '' ? <p className={stylesForm.formError}>{error}</p> : null}
             </form>
         </div >
     )
